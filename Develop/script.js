f492e3191b4aaaffdd1cc387846d96fdd51a9a1e
Michael Li <thisislimichael@gmail.com>
2021-10-16 16:49:06 -0700

    prompt user for length of pw

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,21 @@
 // Assignment code here
+function generatePassword() {
+  // ask user to indicate length of password
+  var promptLength = window.prompt ('Please pick a length (number value) of at least 8 characters and no more than 128 characters');
 
+  // validate prompt answer
+  if (promptLength < 8 || promptLength > 128) {
+    window.alert("Does not meet the numerical requirement! Please try again.");
+    // use return to call function again and stop rest of function from running
+    return generatePassword();
+  }
+
+  else if (isNaN(promptLength)) {
+    window.alert("Please enter a numerical value.");
+    // use return to call function again and stop rest of function from running
+    return generatePassword();
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
