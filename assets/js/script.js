1335ce1fc3d02facb1bc98726bf42f88474acb1e
Michael Li <thisislimichael@gmail.com>
2021-10-23 17:21:27 -0700

    added comments to explain code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
 // Assignment code here 
+
+// Variables are assigned
 var lowerAlph = "abcdefghijklmnopqrstuvwxyz";
 var upperAlph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numericVal = "0123456789";
 var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 var characters = "";
+// this is the array which will hold the characters of the pw
 var passwordCharacters= [];
 
-// 3
+
 function generateCharacters(props) {
   console.log(props);
+  // ask users what they want their pw to be composed of and then stores in variable
   var promptLower = window.confirm('Confirm whether or not to include lowercase characters');
   var promptUpper = window.confirm('Confirm whether or not to include uppercase characters');
   var promptNumeric = window.confirm('Confirm whether or not to include numerical characters');
@@ -17,7 +21,11 @@ function generateCharacters(props) {
   console.log(promptUpper);
   console.log(promptNumeric);
   console.log(promptSpecial);
+
+  // counter to check how many prompts they confirmed to. Counter will go up by one if prompt is true
   var j = 0;
+
+  // the first four characters of pw are determined here and pushed to 'passwordCharacters' array
   if (promptLower == true) {
     j++;
     characters = characters + lowerAlph;
@@ -51,11 +59,13 @@ function generateCharacters(props) {
     console.log(passwordCharacters);
   }
 
+  // if user doesn't select at least one type of character, then they are alerted to do so
   if (j==0) {
     alert("Please choose at least one character type");
     return;
   }
 
+  // the rest of the password is randomly selected here, pushed to the 'passwordCharacters' array and joined to become one single string
   for (i=0; i < props-j; i++) {
     var random = Math.floor(Math.random() * characters.length);
     passwordCharacters.push(characters.charAt(random));
@@ -66,7 +76,7 @@ function generateCharacters(props) {
   return complete;
 }
 
-// 2
+// When the 'Generate Button' button is pressed, user will be asked how long they want their password. generateCharacters function is called here
 function generatePassword() {
   // ask user to indicate length of password
   var promptLength = window.prompt('Please pick a length (number value) of at least 8 characters and no more than 128 characters');
@@ -78,6 +88,7 @@ function generatePassword() {
     return generatePassword();
   }
 
+  // if user puts in a value that is not a number, an alert will show and go back prompt
   else if (isNaN(promptLength)) {
     window.alert("Please enter a numerical value.");
     // use return to call function again and stop rest of function from running
@@ -91,6 +102,7 @@ function generatePassword() {
 
   console.log(promptLength);
 
+  // pw is stored here after the following function is complete
   var password = generateCharacters(promptLength);
   return password;
 }
@@ -99,7 +111,7 @@ function generatePassword() {
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-// 1 
+// password is displayed on screen for user to see
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
